6b2a68df87c6859f4180a71a2928b378c4b95000
Muhammad Anwar <m_anwar@awok.ae>
2017-08-08 16:46:51 +0400

    Clean up

--- routes/web.php
+++ routes/web.php
@@ -10,35 +10,7 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-
-$app->group(['prefix' => 'users'], function ($app) {
-    $app->post('/', 'UsersController@create');
-    $app->group(['middleware' => ['auth', 'role:administrator']], function ($app) {
-        $app->get('/', 'UsersController@index');
-        $app->get('/{id}', 'UsersController@get');
-        $app->get('/{id}/roles', 'UsersController@getUserRoles');
-        $app->put('/{id}', 'UsersController@update');
-        $app->delete('/{id}', 'UsersController@delete');
-    });
+$app->get('/', function () {
+    return 'You\'re here cuz you\'re someone awesome!';
 });
-
-$app->group(['prefix' => 'roles'], function ($app) {
-    $app->group(['middleware' => ['auth', 'role:administrator']], function ($app) {
-        $app->get('/', 'RolesController@index');
-        $app->get('/{id}', 'RolesController@get');
-        $app->post('/', 'RolesController@create');
-        $app->put('/{id}', 'RolesController@update');
-        $app->delete('/{id}', 'RolesController@delete');
-    });
-});
-
-
-$app->group(['prefix' => 'products'], function ($app) {
-    $app->group(['middleware' => ['auth', 'role:administrator']], function ($app) {
-        $app->get('/', 'ProductsController@index');
-        $app->get('/{id}', 'ProductsController@get');
-        $app->post('/', 'ProductsController@create');
-        $app->put('/{id}', 'ProductsController@update');
-        $app->delete('/{id}', 'ProductsController@delete');
-    });
-});
+$app->get('/example', 'ExampleController@index');
