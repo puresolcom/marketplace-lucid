c885c8d383eec2c54dcc34d825072ed494a227cd
Mohammed Raza. Sheik <moh_raza@ho.alifco.ae>
2017-08-14 09:58:44 +0400

    Git clean up cached(.env)

---
+++ app/Domains/ProductsAttribute/Validators/CreateProductsAttributeOptionTranslatableValidator.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Domains\ProductsAttribute\Validators;
+
+use Awok\Validation\Validator;
+
+class CreateProductsAttributeOptionTranslatableValidator extends Validator
+{
+    protected $rules = [
+        /** @todo Add some rules bellow here is one example */
+        // 'email' => 'required|email|unique.users',
+    ];
+}
